5f209d76d6ab8f62efbca7d75441d935408c7bb7
Mifriz <miftahrizki135@gmail.com>
2026-09-13 11:56:13 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/TugasWeb-Pertemuan5-WeatherApp/',
   plugins: [
     tailwindcss(),
   ],
